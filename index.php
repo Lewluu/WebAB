4c77314c71a8b655e00b12ab752b08cdbd694993
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-01-29 13:28:08 +0200

    code update

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 
-require 'src/include/HtmlElement.php';
+include 'src/include/StyleElement.php';
+//include 'src/include/HtmlElement.php';
 
 #copy content from index html
-$html_index=new HtmlElement('src/elements/index.html');
-$html_index->getHTML();
+//$html_index=new HtmlElement();
+//$html_index->setHTML('src/elements/index.html');
+
+#copy content from a css file
+$css_data=new StyleElement();
+$css_data->loadStyle('src/css/main_style.css');
+
+#loat css data in html content
+//$html_index->setStyle($css_data);
+
+//echo $html_index->getHTML();
 
 ?>
--- src/include/HtmlElement.php
+++ src/include/HtmlElement.php
@@ -1,19 +1,53 @@
 <?php
 
+include 'StyleElement.php';
+
 class HtmlElement{
     //attributes
     private $html_data;
+    private $html_path;
+    private StyleElement $style;
     //methods
-    function __construct($path)
-    {
-        $this->html_data=$path;
+    function __construct(){
     }
     function setHTML($path){
         $this->html_data=file_get_contents($path);
+        $this->html_path=$path;
+        // $this->html_data=$path;
+    }
+    function setStyle(StyleElement $style){
+        $this->style=$style;
+        $file_w=fopen("temp_file.html","w");
+        $file_r=fopen($this->html_path,"r");
+        $line_nr=1;
+        while($line=fgets($file_r)){
+            if($line_nr!=8){
+                fputs($file_w,$line);
+                if($line_nr==9)
+                    fputs($file_w,$style->getStyle());
+            }
+            $line_nr++;
+        }
+
+        $this->html_data=file_get_contents("temp_file.html");
+
+        fclose($file_w);
+        fclose($file_r);
+        unlink("temp_file.html");
+    }
+    function getSyle(){
+        return $this->style->getStyle();
     }
     function getHTML(){
-        include $this->html_data;
+        // include $this->html_data;
+        return $this->html_data;
+    }
+    function getHTML_code(){
+        return htmlspecialchars($this->html_data);
+    }
+    function removeHTML(){
     }
 }
+
 
 ?>
