21d0a79ff7b1ac705ea6d95ca6066576b486149c
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-03-27 12:37:35 +0300

    update code

--- src/include/create_project.php
+++ src/include/create_project.php
@@ -4,7 +4,10 @@ if(!empty($_POST)){
     $prj_name=$_POST['project_name'];
     $prj_owner=$_POST['project_owner'];
 
-    echo $prj_name.'<br>'.$prj_owner;
+    $curr_path=getcwd();
+    $curr_path=substr($curr_path,0,-8);
+
+    mkdir($curr_path.'/out/'.$prj_name);
 }
 
 ?>
