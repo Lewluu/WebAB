19b485f956aeb7cff5f92c0e7920ca912d2e9da3
Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@vitesco.com>
2022-03-28 09:31:24 +0300

    html creation update
    Change-Id: I36c60b8666e29161510ea106f405ad39461fde59

--- src/include/HtmlObject.php
+++ src/include/HtmlObject.php
@@ -8,16 +8,16 @@ class HtmlObject extends FileObject{
 
         fwrite($html_file,"<!DOCTYPE html> \n");
         fwrite($html_file,"<html lang='en'> \n");
-        fwrite($html_file,"<head> \n");
-        fwrite($html_file,"<title>".$title."</title> \n");
-        fwrite($html_file,"<meta charset='UTF-8'> \n");
-        fwrite($html_file,"<meta name='viewport' content='width=device-width',initial-scale=1.0> \n");
-        fwrite($html_file,"<meta='description' content=''> \n");
-        fwrite($html_file,"<link rel='stylesheet' type='text/css' href='src/css/style.css'> \n");
-        fwrite($html_file,"<script src='src/scripts/script.js'></script> \n");
-        fwrite($html_file,"</head> \n");
-        fwrite($html_file,"<body> \n");
-        fwrite($html_file,"</body> \n");
+        fwrite($html_file,"\t <head> \n");
+        fwrite($html_file,"\t \t <title>".$title."</title> \n");
+        fwrite($html_file,"\t \t <meta charset='UTF-8'> \n");
+        fwrite($html_file,"\t \t <meta name='viewport' content='width=device-width',initial-scale=1.0> \n");
+        fwrite($html_file,"\t \t <meta='description' content=''> \n");
+        fwrite($html_file,"\t \t <link rel='stylesheet' type='text/css' href='src/css/style.css'> \n");
+        fwrite($html_file,"\t \t <script src='src/scripts/script.js'></script> \n");
+        fwrite($html_file,"\t </head> \n");
+        fwrite($html_file,"\t <body> \n");
+        fwrite($html_file,"\t</body> \n");
         fwrite($html_file,"</html> \n");
 
         fclose($html_file);
